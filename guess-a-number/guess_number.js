b3a25a6218ae7ccf99c7a87ba4cadaf480d77358
Evgeniya <evzibreva@gmail.com>
2024-04-23 18:13:26 +0200

    add min max for guess the number, init modal folder

--- guess-a-number/guess_number.js
+++ guess-a-number/guess_number.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = '🔥 Cowabunga!';
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 19;
-// console.log(document.querySelector('.guess').value);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
