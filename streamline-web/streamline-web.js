da12742fe4d13265e26118e11cd20a29356662a5
Evgeniya <evzibreva@gmail.com>
2025-02-11 17:33:13 +0100

    add cookie banner, DOM learning

--- streamline-web/streamline-web.js
+++ streamline-web/streamline-web.js
@@ -3,51 +3,89 @@
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-btnsOpenModal.forEach(function (btn) {
-  btn.addEventListener('click', openModal);
-});
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
-
 // Adding cooking-banner
+
+// Implementing smooth scrolling
+
+// SELECTING ELEMENTS
 const header = document.querySelector('.header');
 
-const cookieBanner = function () {
-  const cookiesMessage = document.createElement('div');
-  cookiesMessage.classList.add('cookie-message');
-  cookiesMessage.innerHTML = `We use cookies for analytics and to improve functionality of this website <button class="btn btn--close-cookie">Got it!</button>`;
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+console.log(document.getElementById('section--1'));
 
-  header.before(cookiesMessage);
-  // header.insertAdjacentElement('beforebegin', cookiesMessage);
+const allButtons = document.getElementsByTagName('button'); // return live-collection that updates automatically as soon as HTML gets updated
+console.log(allButtons);
 
-  document.querySelector('.btn--close-cookie').addEventListener('click', function () {
-    cookiesMessage.remove();
+const test = document.getElementsByClassName('section'); // return live-collection that updates automatically as soon as HTML gets updated
+console.log(test);
+
+// CREATING & INSERTING ELEMENTS
+const message = document.createElement('div'); // not yet in the DOM, we need to insert it into the page
+message.classList.add('cookie-message');
+message.innerHTML = `We use cookies for analytics and to improve functionality of this website <button class="btn btn--close-cookie">Got it!</button>`;
+
+header.prepend(message);
+// header.insertAdjacentElement('beforebegin', message);
+// header.append(message);
+// header.before(message);
+// header.after(message);
+
+// DELETE ELEMENTS
+const cookieButton = document.querySelector('.btn--close-cookie');
+
+const closeCookieBanner = function () {
+  cookieButton.addEventListener('click', function () {
+    message.remove();
   });
 };
 
-cookieBanner();
+closeCookieBanner();
 
-// Implementing smooth scrolling
+// ---------- STYLES ----------
+
+// INLINE STYLES
+
+// adding
+message.style.backgroundColor = '#444444';
+message.style.width = '120%';
+
+// getting styles
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 4 + 'px';
+
+// CUSTOM STYLES (the ones defined in root)
+document.documentElement.style.setProperty('--color-primary', 'violet');
+
+// ATTRIBUTES
+
+// Get attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt); // gives you absolute URL (http & everything)
+console.log(logo.src); // gives you relative URL (folder & name)
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // absolute value
+console.log(link.getAttribute('href')); // value like in HTML
+
+console.log(logo.getAttribute('src'));
+console.log(logo.className); // shows css-class of teh element
+// NOTE: js can only display the names of standard attributes, not random custom created one. Eg: if we create an attribute "designer" on our logo-image it can't be looked up like src or alt (will return 'undefined')
+console.log(logo.designer);
+// there is a way of getting custom attributes:
+console.log(logo.getAttribute('designer'));
+
+// Set attributes
+logo.alt = 'Minimalist logo'; // changing existing one
+logo.setAttribute('company', 'Bankist'); // creating new one
+
+// DATA-ATTRIBUTES
+console.log(logo.dataset.versionNumber); // use dataset to call out data-attribute & camelCase for the data-property name
+
+// CLASSES
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
